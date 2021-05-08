457bbcd5846cb6132844542c9a21b1829c9fc4bb
Mateo LOSTANLEN <mateo.lostanlen@gmail.com>
2021-05-09 00:09:20 +0200

    freeze

--- utils.py
+++ utils.py
@@ -36,18 +36,9 @@ def freeze_model(model: Module, last_frozen_layer: Optional[str] = None, frozen_
 
     # Loop on parameters
     if isinstance(last_frozen_layer, str):
-        layer_reached = False
+  
         for n, p in model.named_parameters():
             if n.startswith(last_frozen_layer):
-                layer_reached = True
-                p.requires_grad_(False)
-            elif not layer_reached:
-                p.requires_grad_(False)
-        if not layer_reached:
-            raise ValueError(f"Unable to locate child module {last_frozen_layer}")
-
-    # Loop on modules
-    if not frozen_bn_stat_update:
-        model = freeze_bn(model)
+                p.requires_grad_(True)
 
     return model
